6390b0f4ab9a1b9159f3754c9f1dce9391ab2d80
Vansh Rajput <135845531+vanshrajput099@users.noreply.github.com>
2024-12-30 21:33:49 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/chat-app-frontend/',
-  build: {
-    outDir: 'dist',
-  },
   server: {
     proxy: {
       '/api': 'https://chat-app-backend-bo1l.onrender.com',
